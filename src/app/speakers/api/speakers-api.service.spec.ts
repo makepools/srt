4340ac798131e87f650f3880b038c23884408adc
makepool <makepool@hotmail.com>
2024-04-29 15:06:07 +0100

    test(srt): add tests for the speakers API service

---
+++ src/app/speakers/api/speakers-api.service.spec.ts
@@ -0,0 +1,46 @@
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+
+import { SpeakersApiService } from './speakers-api.service';
+import { MOCK_PAGINATED_SPEAKERS } from '../mocks';
+
+const setup = () => {
+  TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule],
+    providers: [SpeakersApiService],
+  });
+
+  const service = TestBed.inject(SpeakersApiService);
+  const httpTestingController = TestBed.inject(HttpTestingController);
+
+  return {
+    service,
+    httpTestingController,
+  };
+};
+
+describe('SpeakersApiService', () => {
+  describe('getPaginated', () => {
+    it('returns data on success', (done) => {
+      const { service, httpTestingController } = setup();
+
+      service.getPaginated().subscribe((data) => {
+        expect(data).toEqual(MOCK_PAGINATED_SPEAKERS);
+        done();
+      });
+
+      const req = httpTestingController.expectOne(
+        `https://randomuser.me/api/1.4/?page=0&results=20&seed=srt`
+      );
+
+      expect(req.request.method).toEqual('GET');
+
+      req.flush(MOCK_PAGINATED_SPEAKERS);
+
+      httpTestingController.verify();
+    });
+  });
+});
