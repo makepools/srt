9049714ab94484d407f2ed66b36accedeecc3ae1
makepool <makepool@hotmail.com>
2024-04-29 14:52:36 +0100

    chore(srt) install jest for testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'srt' title`, () => {
+  it(`should have the 'SRT' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('srt');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, srt');
+    expect(app.title).toEqual('SRT');
   });
 });
